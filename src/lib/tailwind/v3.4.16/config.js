ce16a64b7519eecb0ca1a04fdb007938627c25d8
¡SomethingSpecial! <78428114+wo-r@users.noreply.github.com>
2026-09-21 13:01:23 -0500

    I like this formatting a lot

--- src/lib/tailwind/v3.4.16/config.js
+++ src/lib/tailwind/v3.4.16/config.js
@@ -8,30 +8,75 @@
      * Manages the fonts that can be used via Tailwind.
      * Use: Add font, then use font-[Name Of Font] in html classings.
      */
-    var fontFamilys = {
-        rubik: ["Rubik", "sans-serif"],
-        rubikBlack: ["RubikBlack", "sans-serif"],
-        rubikBold: ["RubikBold", "sans-serif"],
-        rubikMedium: ["RubikMedium", "sans-serif"],
-        rubikSemiBold: ["RubikSemiBold", "sans-serif"],
-        pamela: ["Pamela", "sans-serif"]
+    var fonts = {
+        ubuntu: ["Ubuntu", "sans-serif"],
     }
 
-    //unused
-    var colorPalatte = {}
+    /**
+     * Add custom color attributes
+     * Use: name-of-color: color HEX,
+     */
+    var colors = {}
+
+    /**
+     * Creates all custom classes and initalize font-sets.
+     */
+    var plugins = [
+        function ({ addBase, addUtilities }) {
+            // Font-sets
+            addBase({
+                //ubuntu
+                '@font-face': {
+                    'font-family': 'Ubuntu',
+                    'src': 'url("/src/res/fonts/Ubuntu/static/Ubuntu-Regular.ttf") format("truetype")',
+                    'font-weight': '400',
+                    'font-style': 'normal',
+                    'font-display': 'swap',
+                },
+            });
+            // CSS Classes
+            addUtilities({
+                // scrollbar
+                '.scrollbar-hidden': {
+                    'scrollbar-width': 'none',
+                    '-ms-overflow-style': 'none',
+                },
+                '.scrollbar-hidden::-webkit-scrollbar': {
+                    width: '0px',
+                    height: '0px',
+                },
+                '.scrollbar-hidden::-webkit-scrollbar-track': {
+                    background: 'transparent',
+                },
+                '.scrollbar-hidden::-webkit-scrollbar-thumb': {
+                    background: 'transparent',
+                },
+                // selection
+                '.selection': {
+                    '&::selection': {
+                        background: '#9ff0f3ca',
+                        color: 'black',
+                    },
+                },
+            })
+        },
+    ]
 
     //unused
-    var extraOptions = {}
+    var extras = {}
 
     tailwind.config = {
+        plugins: plugins,
+        
         theme: {
             extend: {
-                fontFamily: fontFamilys,
-                colors: colorPalatte,
+                fontFamily: fonts,
+                colors: colors,
                 animation: {},
                 keyframes: {},
             },
-            extraOptions
+
+            extras
         }
     }
 })();
